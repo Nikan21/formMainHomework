41d991234fe4a3d4a5c28e7ff31ac0204d886813
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-17 19:14:59 +0300

    Create logotype component

---
+++ src/Components/LoginPage/LoginLogo/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logotype from '../../../assets/images/logo.png'
+
+export default function Logo() {
+    return (
+        <a href="https://www.squadhelp.com/">
+            <img src={Logotype} alt="Logo" />
+        </a>
+    )
+}

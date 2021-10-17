d7e6ebf5d320d5adea7f94fd51eafdcdcc62dde3
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-17 18:51:09 +0300

    Add routing tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import LoginPage from './Components/LoginPage/index'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Link to="/login">Login Page</Link>
+      <Link to="/signUp">Sign Up</Link>
+      <Switch>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/signUp"></Route>
+      </Switch>
+    </Router>
   );
 }
 

e2569ce13c807845bf807927b37545ad97430c49
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-17 20:09:33 +0300

    Create mainText component

---
+++ src/Components/LoginPage/LoginMainText/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './loginMainText.sass'
+
+export default function MainText() {
+    return (
+        <h1 className = 'loginMainText'>
+            LOGIN TO YOUR ACCOUNT
+        </h1>
+    )
+}

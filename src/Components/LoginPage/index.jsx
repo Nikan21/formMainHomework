3c2ff0d9dcf8fe22710a4b35452ff03c1c525a09
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-17 19:15:50 +0300

    Add logo component

---
+++ src/Components/LoginPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from './LoginLogo/index'
+
+export default function LoginPage() {
+    return (
+        <body>
+            <Logo />
+            
+        </body>
+    )
+}

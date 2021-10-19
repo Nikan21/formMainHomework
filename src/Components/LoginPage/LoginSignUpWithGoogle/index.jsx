87f610d0096b5d74b5f261d69c4eea0421667123
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-19 23:04:53 +0300

    Create SignUpWithGoogle component

---
+++ src/Components/LoginPage/LoginSignUpWithGoogle/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SignUpWithGoogle() {
+  return (
+    <a href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?response_type=code&redirect_uri=https%3A%2F%2Fwww.squadhelp.com%2Flogin.php&client_id=78274772730-f62839c388e8b785mhfh211vr4ueva08.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&access_type=offline&approval_prompt=auto&flowName=GeneralOAuthFlow">Sign in with Google</a>
+  );
+}

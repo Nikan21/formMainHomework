b10d9ef8c6694278055b53e860d51675da8df432
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-10-19 18:39:13 +0300

    Add function OnSubmit

--- src/Components/LoginPage/LoginForm/index.jsx
+++ src/Components/LoginPage/LoginForm/index.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
 
-const initialValues = {
-  email: "",
-  password: "",
-  checkbox: false,
-};
-
 export default function LoginForm() {
+
+  const initialValues = {
+    email: "",
+    password: "",
+    checkbox: false,
+  };
+  
+  const onSumbit = ((values, formikBag) =>{
+    console.log(values)
+    formikBag.resetForm()
+  })
+
   return (
     <Formik
       initialValues={initialValues}
-      onSubmit={(values, formikBag) => {}}
+      onSubmit={onSumbit}
     >
         {(formikProps) =>{
             return(
                 <Form>
-                    <Field classNmae = 'emailInput' name = 'email'></Field>
-                    <Field classNmae = 'passwordInput' name = 'password' type = 'password'></Field>
+                    <Field className = 'emailInput' name = 'email'></Field>
+                    <Field className = 'passwordInput' name = 'password' type = 'password'></Field>
                     <Field name = 'checkbox' type = 'checkbox'></Field>
                     <a href="https://www.squadhelp.com/forgot_password.php">Forgot Password</a>
                     <button className = 'loginButton' type = 'submit'>LOGIN</button>

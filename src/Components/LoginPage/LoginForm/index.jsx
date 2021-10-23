a782d00c65c0742a7ffce8fe90b334703c84b341
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-23 18:12:25 +0300

    Update form

--- src/Components/LoginPage/LoginForm/index.jsx
+++ src/Components/LoginPage/LoginForm/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage} from "formik";
 import * as yup from 'yup'
-import styles from './Form.module.sass';
+import classNames from 'classnames'
+import styles from './form.module.sass';
 
 export default function LoginForm() {
 
@@ -17,9 +18,8 @@ export default function LoginForm() {
   })
 
   const scheme = yup.object().required().shape({
-    email: yup.string().email().required('Field is empty'),
-    password: yup.string().matches(/[A-z][a-z][0-9]/).required('Field is empty'),
-    checkbox: yup.boolean().required('Field is empty')
+    email: yup.string().matches(/[A-z][a-z][0-9][.@]/, 'Email is not valid format').required('Field is empty'),
+    password: yup.string().matches(/[A-z][a-z][0-9]/, 'Password is not valid').required('Field is empty'),
   })
 
   return (
@@ -27,17 +27,47 @@ export default function LoginForm() {
       initialValues={initialValues}
       onSubmit={onSumbit}
       validationSchema = {scheme}
+      
     >
-        {(formikProps) =>{
+        {({errors, touched}) =>{
+          
+          const errorEmails = classNames (styles.errorEmailDefault,{
+            [styles.errorEmail]: errors.email
+          })
+
+          const colorInputEmail = classNames(styles.emailInput,{
+            [styles.errorEmailTrue]: !errors.email && touched.email,
+            [styles.errorEmailFalse]: errors.email && touched.email
+          })
+
+          const errorPassword = classNames (styles.errorPasswordDefault,{
+            [styles.errorPassword]: errors.password
+          })
+
+          const colorInputPassword = classNames(styles.passwordInput,{
+            [styles.errorPasswordTrue]: !errors.password && touched.password,
+            [styles.errorPasswordFalse]: errors.password && touched.password
+          })
+
             return(
                 <Form className= {styles.form}>
-                    <Field className = {styles.emailInput} name = 'email' placeholder = 'Email adress'/>
-                    <Field className = {styles.passwordInput} name = 'password' type = 'password' placeholder = 'Password'/>
+
+                  <label className = {styles.labelEmail}>
+                    <Field className = {colorInputEmail} name = 'email' type = 'text' placeholder = 'Email adress'/>
+                    <div className = {errorEmails}><ErrorMessage  name = 'email'/></div>
+                    </label>
+
+                    <label className = {styles.labelPassword}>
+                    <Field className = {colorInputPassword} name = 'password' type = 'password' placeholder = 'Password'/>
+                    <div className = {errorPassword}><ErrorMessage  name = 'password'/></div>
+                    </label>
+                    
                     <div className = {styles.wrapperCheckAndText}>
                     <Field name = 'checkbox' type = 'checkbox'/>
                     <span className = {styles.textNearCheckbox}>Remember Me</span>
                     <a className = {styles.forgotPass} href="https://www.squadhelp.com/forgot_password.php">Forgot Password</a>
                     </div>
+                    
                     <button className = {styles.loginButton} type = 'submit'>LOGIN</button>
                 </Form>
             )

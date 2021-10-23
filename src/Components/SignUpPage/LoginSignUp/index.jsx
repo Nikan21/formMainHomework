7e7762f02280ae362a6b5393675b1e3bb5aba119
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-24 00:08:59 +0300

    Create signUpButton

---
+++ src/Components/SignUpPage/LoginSignUp/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./signButton.module.sass";
+
+export default function SignUpButton() {
+  return (
+    <a className={styles.signUpButton} href="/login">
+      Login
+    </a>
+  );
+}

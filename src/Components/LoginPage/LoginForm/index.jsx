a0846be597af4c3bd752f4eeaa66775a4bd0db6e
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-23 10:49:00 +0300

    Add not working yup

--- src/Components/LoginPage/LoginForm/index.jsx
+++ src/Components/LoginPage/LoginForm/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
+import * as yup from 'yup'
 import styles from './Form.module.sass';
 
 export default function LoginForm() {
@@ -15,18 +16,25 @@ export default function LoginForm() {
     formikBag.resetForm()
   })
 
+  const scheme = yup.object().required().shape({
+    email: yup.string().email().required('Field is empty'),
+    password: yup.string().matches(/[A-z][a-z][0-9]/).required('Field is empty'),
+    checkbox: yup.boolean().required('Field is empty')
+  })
+
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={onSumbit}
+      validationScheme = {scheme}
     >
         {(formikProps) =>{
             return(
                 <Form className= {styles.form}>
-                    <Field className = {styles.emailInput} name = 'email' placeholder = 'Email adress' required></Field>
-                    <Field className = {styles.passwordInput} name = 'password' type = 'password' placeholder = 'Password' required></Field>
+                    <Field className = {styles.emailInput} name = 'email' placeholder = 'Email adress'/>
+                    <Field className = {styles.passwordInput} name = 'password' type = 'password' placeholder = 'Password'/>
                     <div className = {styles.wrapperCheckAndText}>
-                    <Field name = 'checkbox' type = 'checkbox' required></Field>
+                    <Field name = 'checkbox' type = 'checkbox'/>
                     <span className = {styles.textNearCheckbox}>Remember Me</span>
                     <a className = {styles.forgotPass} href="https://www.squadhelp.com/forgot_password.php">Forgot Password</a>
                     </div>

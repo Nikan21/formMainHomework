643f178785802bcae96a0b1d16907d95e30d1d5e
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-24 19:25:43 +0300

    Fixed text in link

--- src/Components/SignUpWithGoogle/index.jsx
+++ src/Components/SignUpWithGoogle/index.jsx
@@ -10,7 +10,7 @@ export default function SignUpWithGoogle() {
         className={styles.linkGoogle}
         href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?response_type=code&redirect_uri=https%3A%2F%2Fwww.squadhelp.com%2Flogin.php&client_id=78274772730-f62839c388e8b785mhfh211vr4ueva08.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&access_type=offline&approval_prompt=auto&flowName=GeneralOAuthFlow"
       >
-        Sign in with Google
+        Sign up with Google
       </a>
     </div>
   );

0dfeadace88e8694555b9c60c1642acb7d46d686
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-10-24 13:09:54 +0300

    Add component

--- src/Components/SignUpPage/index.jsx
+++ src/Components/SignUpPage/index.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Logo from "../HeaderLogo/index";
 import SignUpButton from "./LoginSignUp/index";
 import SignUpMainText from './SignUpMainText/index'
-import SignUpForm from "./SignUpForm";
+import SignUpForm from "./SignUpForm/index";
+import SignUpWithGoogle from '../SignUpWithGoogle/index'
 
 import styles from "./signUpPage.module.sass";
 
 export default function SignUpPage() {
   return (
-    <div className={styles.loginWindow}>
+    <>
+    <div className={styles.loginWindowOne}>
       <header className={styles.header}>
         <Logo />
         <SignUpButton />
@@ -16,8 +18,11 @@ export default function SignUpPage() {
       <main className={styles.main}>
         <SignUpMainText />
         <SignUpForm />
+        <SignUpWithGoogle />
       </main>
       <footer className={styles.footer}></footer>
     </div>
+    <div className={styles.loginWindowTwo}></div>
+    </>
   );
 }
